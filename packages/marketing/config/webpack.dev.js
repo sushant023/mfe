c09fc9c700ee179c411e0dc861c0ecd7813fef6e
sushant023 <sushantbhaskar23@gmail.com>
2023-05-17 03:03:16 +0530

    Created container deploy script

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,9 +1,10 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const packageJson = require("../package.json");
 
+const packageJson = require("../package.json");
 const commonConfig = require("./webpack.common");
+
 const devConfig = {
   mode: "development",
   devServer: {
