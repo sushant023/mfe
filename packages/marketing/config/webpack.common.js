4e5d1628415ea7abdc1f35237618a292bd2c3e8a
Sushant <sushant@Sushants-MacBook-Air.local>
2023-05-17 01:59:06 +0530

    Initial Commit

---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,21 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
+
+//"@babel/preset-react" - For react specific codes like jsx
+//"@babel/plugin-transform-runtime" -browser specific like promises, async await
+//"@babel/preset-env" - ES6 to ES5
